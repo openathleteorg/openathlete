c892893b8cd24356e4494bbdadc9572b20516e29
Tristan Bouillot <tristanblt@gmail.com>
2025-12-02 09:48:15 +0100

    feat(database): add require_feedback_questions to athlete_settings and feedback_skipped to event_activity for improved feedback management

---
+++ libs/database/prisma/schema/migrations/20251202082338_add_skip_to_feedbacks/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."event_activity" ADD COLUMN     "feedback_skipped" BOOLEAN NOT NULL DEFAULT false;
---
+++ libs/database/prisma/schema/migrations/20251202083741_add_require_feedback_questions/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."athlete_settings" ADD COLUMN     "require_feedback_questions" BOOLEAN NOT NULL DEFAULT true;
