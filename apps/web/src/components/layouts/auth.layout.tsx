abc05ffae0731cc30fc2c7980a0ceebcedc3c894
Tristan Bouillot <tristanblt@gmail.com>
2025-12-04 12:52:37 +0100

    fix(web): update login link to use dynamic website URL and reintroduce language switcher component

--- apps/web/src/components/layouts/auth.layout.tsx
+++ apps/web/src/components/layouts/auth.layout.tsx
@@ -1,6 +1,6 @@
-import { LanguageSwitcher } from '@/components/language-switcher';
 import authBackground from '@/assets/images/auth/background.webp';
 import whiteLogoSrc from '@/assets/logos/logo_white.svg';
+import { LanguageSwitcher } from '@/components/language-switcher';
 import { Link } from 'react-router-dom';
 
 interface P {
@@ -8,12 +8,16 @@ interface P {
 }
 
 export function AuthLayout({ children }: P) {
+  const websiteUrl = process.env.VITE_WEBSITE_URL || '/';
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex items-center justify-between gap-4">
           <div className="flex flex-1 justify-start gap-2">
-            <Link to="/" className="flex items-center gap-2 font-medium">
+            <Link
+              to={websiteUrl}
+              className="flex items-center gap-2 font-medium"
+            >
               <div className="flex h-8 w-8 items-center justify-center rounded-md bg-[var(--oa-bg)] text-primary-foreground">
                 <img src={whiteLogoSrc} alt="Logo" className="h-6 w-6" />
               </div>
