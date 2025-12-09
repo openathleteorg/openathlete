d53ebb00990cfdf5f0b20274519b3d6b62d651ce
Tristan Bouillot <tristanblt@gmail.com>
2025-12-09 13:58:49 +0100

    fix(api): update Suunto webhook secret to subscription key for improved API integration

--- apps/api/scripts/docker-entrypoint.sh
+++ apps/api/scripts/docker-entrypoint.sh
@@ -81,7 +81,7 @@ decode_secret "GOOGLE_GENERATIVE_AI_API_KEY"
 decode_secret "STRIPE_SECRET_KEY"
 decode_secret "STRIPE_WEBHOOK_SECRET"
 decode_secret "SUUNTO_CLIENT_SECRET"
-decode_secret "SUUNTO_WEBHOOK_SECRET_KEY"
+decode_secret "SUUNTO_SUBSCRIPTION_KEY"
 
 cd /app/libs/database
 
